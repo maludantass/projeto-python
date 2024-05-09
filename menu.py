54901f7c0d55db2089304ac60ce171023133f0d8
Malu Dantas <maludantass123@gmail.com>
2024-05-09 08:41:56 -0300

    teste

---
+++ menu.py
@@ -0,0 +1,16 @@
+
+cadastros = {}
+
+
+while True:
+    print('\n''\n''█▀▄▀█ █▀▀ █▄░█ █░█ \n''█░▀░█ ██▄ █░▀█ █▄█''\n''\n''\n''█▀█ █▀█ █ █▄░█ █▀▀ █ █▀█ ▄▀█ █░░''\n''█▀▀ █▀▄ █ █░▀█ █▄▄ █ █▀▀ █▀█ █▄▄''\n''\n'
+          'OPÇÕES:''\N'
+          '[1]: ADICIONAR'
+          '[2]: VISUALIZAR'
+          '[3]: DELETAR'
+          '[4]: ATUALIZAR'
+          '[5]: SALVAR'
+          '[6]: SAIR'
+          '[7]: teste2'
+          )
+    
